51b03b36ac1643fda679608c4d352e2cb2866fcf
Sankalp Sandeep Paranjpe <77071390+sankalpsp07@users.noreply.github.com>
2023-03-06 15:53:53 +0530

    Update Assignment3.1 Temperature.tcl

--- Assignment3.1 Temperature.tcl
+++ Assignment3.1 Temperature.tcl
@@ -1,1 +1,24 @@
+puts "Temperature Conversion\n"
+puts "Enter \n '1' for F to C\n '2' for C to F"
+gets stdin ch
+switch $ch {
 
+"1" { 
+	puts "Enter temperature in Fahrenheit"
+	gets stdin fdeg
+	set cdeg [expr ($fdeg -32.0) * 5/9]
+	puts "In Celcius, the degree is $cdeg degrees"
+}
+
+"2" {
+	puts "Enter temperature in Celcius"
+	gets stdin cdeg
+	set fdeg [expr ($cdeg *9 / 5)+ 32.0]
+	puts "In Fahrenheit, the degree is $fdeg degrees"
+}
+
+default {
+	puts "Invalid Input"
+}
+
+}
